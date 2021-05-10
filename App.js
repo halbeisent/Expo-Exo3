9f872ea3a1121eb5bebb75069568dc98eef4cee7
halbeisent <thibault.halbeisendev@gmail.com>
2021-05-10 15:31:40 +0200

    Exercice 3 à l'introduction d'Expo terminé

--- App.js
+++ App.js
@@ -4,16 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  <View style={{flex: 1}}>
+    <View style={{flex: 2, flexDirection: "row"}}>
+      <View style={{flex: 3, backgroundColor: "green"}}></View>
+      <View style={{flex: 2, backgroundColor: "red"}}></View>
+      <View style={{flex: 1, backgroundColor: "blue"}}></View>
       <StatusBar style="auto" />
     </View>
+      <View style={{flex: 3}}>
+      <View style={{flex: 3, backgroundColor: "purple"}}></View>
+      <View style={{flex: 2, backgroundColor: "yellow"}}></View>
+      <View style={{flex: 1, backgroundColor: "pink"}}></View>
+    </View>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
